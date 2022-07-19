687502f61629120beedd679972694be99c6776d1
Adam Hultman <ahultman0@gmail.com>
2022-07-18 19:02:38 -0700

    add comments, basic funcs

--- example.c
+++ example.c
@@ -21,8 +21,11 @@ int main(){
 
 int readWaveHeader(FILE *ptr){
     int read = 0;
+    // RIFF
     read = fread(header.riff, sizeof(header.riff), 1, ptr);
     printf("(1-4): %s \n", header.riff);
+
+    // SIZE
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
     printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
     
@@ -33,9 +36,11 @@ int readWaveHeader(FILE *ptr){
                         (buffer4[3]<<24);
     printf("(5-8) Overall size: bytes:%u, Kb:%u \n", header.overall_size, header.overall_size/1024);
     
+    // WAVE
     read = fread(header.wave, sizeof(header.wave), 1, ptr);
     printf("(9-12) Wave marker: %s\n", header.wave);
     
+    // fmt 
     read = fread(header.fmt_chunk_marker, sizeof(header.fmt_chunk_marker), 1, ptr);
     printf("(13-16) Fmt marker: %s\n", header.fmt_chunk_marker);
     
@@ -61,10 +66,13 @@ int readWaveHeader(FILE *ptr){
         strcpy(format_name, "Mu-law");
     printf("(21-22) Format type: %u %s \n", header.format_type, format_name);
     
+    // fmt channels
     read = fread(buffer2, sizeof(buffer2), 1, ptr);
     printf("%u %u \n", buffer2[0], buffer2[1]);
     header.channels = buffer2[0] | (buffer2[1] << 8);
     printf("(23-24) Channels: %u \n", header.channels);
+
+    // fmt sample rate
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
     printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
     header.sample_rate = buffer4[0] |
@@ -72,6 +80,8 @@ int readWaveHeader(FILE *ptr){
                         (buffer4[2] << 16) |
                         (buffer4[3] << 24);
     printf("(25-28) Sample rate: %u\n", header.sample_rate);
+
+    // fmt byterate
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
     printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
     header.byterate  = buffer4[0] |
@@ -80,18 +90,25 @@ int readWaveHeader(FILE *ptr){
                         (buffer4[3] << 24);
     printf("(29-32) Byte Rate: %u , Bit Rate:%u\n", header.byterate, header.byterate*8);
 
+    // block alignment
     read = fread(buffer2, sizeof(buffer2), 1, ptr);
     printf("%u %u \n", buffer2[0], buffer2[1]);
     header.block_align = buffer2[0] |
                     (buffer2[1] << 8);
     printf("(33-34) Block Alignment: %u \n", header.block_align);
+
+    // bits per sample
     read = fread(buffer2, sizeof(buffer2), 1, ptr);
     printf("%u %u \n", buffer2[0], buffer2[1]);
     header.bits_per_sample = buffer2[0] |
                     (buffer2[1] << 8);
     printf("(35-36) Bits per sample: %u \n", header.bits_per_sample);
+
+    // chunk header marker
     read = fread(header.data_chunk_header, sizeof(header.data_chunk_header), 1, ptr);
     printf("(37-40) Data Marker: %s \n", header.data_chunk_header);
+
+    // chunk size
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
     printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
     header.data_size = buffer4[0] |
@@ -99,6 +116,7 @@ int readWaveHeader(FILE *ptr){
                     (buffer4[2] << 16) |
                     (buffer4[3] << 24 );
     printf("(41-44) Size of data chunk: %u \n", header.data_size);
+
     // calculate no.of samples
     long num_samples = (8 * header.data_size) / (header.channels * header.bits_per_sample);
     printf("Number of samples:%lu \n", num_samples);
@@ -106,8 +124,7 @@ int readWaveHeader(FILE *ptr){
     long size_of_each_sample = (header.channels * header.bits_per_sample) / 8;
     printf("Size of each sample:%ld bytes\n", size_of_each_sample);
 
- // calculate duration of file
-
+    // calculate duration of file
     float duration_in_seconds = (float) header.overall_size / header.byterate;
     printf("Approx.Duration in seconds=%f\n", duration_in_seconds);
 }
@@ -118,5 +135,75 @@ void readWaveFileSamples(FILE *ptr){
     }else{
         printf("Can only read PCM.");
         exit(1);
+    }
+}
+
+int signum ( int sample ) {
+    if( sample < 0)
+        return( 0); /* sign is ’0’ for negative samples */
+    else
+        return( 1); /* sign is ’1’ for positive samples */
+}
+
+int magnitude ( int sample ) {
+    if( sample < 0) {
+        sample = - sample ;
+    }
+    return( sample );
+}
+
+
+char codeword_compression ( unsigned int sample_magnitude, int sign ) {
+    int chord, step;
+    int codeword_tmp;
+
+    if( sample_magnitude & (1 << 12)) {
+        chord = 0 x7 ;
+        step = ( sample_magnitude >> 8) & 0 xF ;
+        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        return ( (char) codeword_tmp );
+    }
+    if( sample_magnitude & (1 << 11)) {
+        chord = 0 x6 ;
+        step = ( sample_magnitude >> 7) & 0 xF ;
+        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        return ( (char) codeword_tmp );
+    }
+    if( sample_magnitude & (1 << 10)) {
+        chord = 0 x5 ;
+        step = ( sample_magnitude >> 6) & 0 xF ;
+        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        return ( (char) codeword_tmp );
+    }
+
+    if( sample_magnitude & (1 << 9)) {
+        chord = 0 x4 ;
+        step = ( sample_magnitude >> 5) & 0 xF ;
+        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        return ( (char) codeword_tmp );
+    }
+    if( sample_magnitude & (1 << 8)) {
+        chord = 0 x3 ;
+        step = ( sample_magnitude >> 4) & 0 xF ;
+        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        return ( (char) codeword_tmp );
+    }
+    if( sample_magnitude & (1 << 7)) {
+        chord = 0 x2 ;
+        step = ( sample_magnitude >> 3) & 0 xF ;
+        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        return ( (char) codeword_tmp );
+    }
+    if( sample_magnitude & (1 << 6)) {
+        chord = 0 x1 ;
+        step = ( sample_magnitude >> 2) & 0 xF ;
+        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        return ( (char) codeword_tmp );
+    }
+    if( sample_magnitude & (1 << 5)) {
+        chord = 0 x0 ;
+        step = ( sample_magnitude >> 1) & 0 xF ;
+        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        return ( (char) codeword_tmp );
     }
 }
