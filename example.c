9089e284325289060933a5420bb9ecfac32bd72f
Adam Hultman <ahultman0@gmail.com>
2022-07-27 20:22:31 -0700

    fix compression, add write

--- example.c
+++ example.c
@@ -1,146 +1,229 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+//#include <unistd.h>
 #include <stdlib.h>
 #include "example.h"
 
 FILE *ptr;
 char *filename;
-struct WAVE_HEADER header;
-struct WAVE_FORMAT_CHUNK fmt;
-struct WAVE_DATA_CHUNK data_chunk; // temp
-struct WAVE_DATA_CHUNK_COMPRESSED compressed_data; // temp
+struct WAVE wave;
+struct WAVE_DATA_CHUNK_COMPRESSED compressed_data;
 unsigned char buffer4[4];
 unsigned char buffer2[2];
+
+void compressData();
+void decompressData();
+
+void writeWaveFile(char *);
 
 long num_samples;
 long size_of_each_sample;
 
-int main(){
-    ptr = fopen("test.wav", "rb");
+int main( int argc, char *argv[] ) {
+    filename = argv[1];
+    ptr = fopen(filename, "rb");
     if(ptr == NULL){
         printf("Error opening file\n");
         exit(1);
     }
+
     readWaveHeader(ptr);
+    compressData();
+    decompressData();
+    writeWaveFile("decompressed.wav");
 }
 
 int readWaveHeader(FILE *ptr){
     int read = 0;
     
     // RIFF
-    read = fread(header.riff, sizeof(header.riff), 1, ptr);
-    printf("(1-4): %s \n", header.riff);
+    read = fread(wave.waveHeader.riff, sizeof(wave.waveHeader.riff), 1, ptr);
+    printf("(1-4): %s \n", wave.waveHeader.riff);
 
     // SIZE
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
     printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
     
     // convert little endian to big endian 4 byte int
-    header.overall_size  = buffer4[0] |
+    wave.waveHeader.fileSize  = buffer4[0] |
                         (buffer4[1]<<8) |
                         (buffer4[2]<<16) |
                         (buffer4[3]<<24);
-    printf("(5-8) Overall size: bytes:%u, Kb:%u \n", header.overall_size, header.overall_size/1024);
+    printf("(5-8) Overall size: bytes:%u, Kb:%u \n", wave.waveHeader.fileSize, wave.waveHeader.fileSize/1024);
     
     // WAVE
-    read = fread(header.wave, sizeof(header.wave), 1, ptr);
-    printf("(9-12) Wave marker: %s\n", header.wave);
+    read = fread(wave.waveHeader.wave, sizeof(wave.waveHeader.wave), 1, ptr);
+    printf("(9-12) Wave marker: %s\n", wave.waveHeader.wave);
     
-    // fmt 
-    read = fread(fmt.chunkType, sizeof(fmt.chunkType), 1, ptr);
-    printf("(13-16) Fmt marker: %s\n", fmt.chunkType);
+    // wave.waveFormatChunk 
+    read = fread(wave.waveFormatChunk.chunkType, sizeof(wave.waveFormatChunk.chunkType), 1, ptr);
+    printf("(13-16) wave.waveFormatChunk marker: %s\n", wave.waveFormatChunk.chunkType);
     
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
     printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
 
     // convert little endian to big endian 4 byte integer
-    fmt.chunkSize = buffer4[0] |
+    wave.waveFormatChunk.chunkSize = buffer4[0] |
                             (buffer4[1] << 8) |
                             (buffer4[2] << 16) |
                             (buffer4[3] << 24);
-    printf("(17-20) Length of Fmt header: %u \n", fmt.chunkSize);
+    printf("(17-20) Length of wave.waveFormatChunk wave.waveHeader: %u \n", wave.waveFormatChunk.chunkSize);
     
     read = fread(buffer2, sizeof(buffer2), 1, ptr); printf("%u %u \n", buffer2[0], buffer2[1]);
-    fmt.audioFormat = buffer2[0] | (buffer2[1] << 8);
+    wave.waveFormatChunk.audioFormat = buffer2[0] | (buffer2[1] << 8);
 
     char format_name[10] = "";
-    if (fmt.audioFormat == 1)
+    if (wave.waveFormatChunk.audioFormat == 1)
         strcpy(format_name,"PCM");
-    else if (fmt.audioFormat == 6)
+    else if (wave.waveFormatChunk.audioFormat == 6)
         strcpy(format_name, "A-law");
-    else if (fmt.audioFormat == 7)
+    else if (wave.waveFormatChunk.audioFormat == 7)
         strcpy(format_name, "Mu-law");
-    printf("(21-22) Format type: %u %s \n", fmt.audioFormat, format_name);
+    printf("(21-22) Format type: %u %s \n", wave.waveFormatChunk.audioFormat, format_name);
     
-    // fmt channels
+    // wave.waveFormatChunk channels
     read = fread(buffer2, sizeof(buffer2), 1, ptr);
     printf("%u %u \n", buffer2[0], buffer2[1]);
-    fmt.numChannels = buffer2[0] | (buffer2[1] << 8);
-    printf("(23-24) Channels: %u \n", fmt.numChannels);
+    wave.waveFormatChunk.numChannels = buffer2[0] | (buffer2[1] << 8);
+    printf("(23-24) Channels: %u \n", wave.waveFormatChunk.numChannels);
 
-    // fmt sample rate
+    // wave.waveFormatChunk sample rate
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
     printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
-    fmt.sampleRate = buffer4[0] |
+    wave.waveFormatChunk.sampleRate = buffer4[0] |
                         (buffer4[1] << 8) |
                         (buffer4[2] << 16) |
                         (buffer4[3] << 24);
-    printf("(25-28) Sample rate: %u\n", fmt.sampleRate);
+    printf("(25-28) Sample rate: %u\n", wave.waveFormatChunk.sampleRate);
 
-    // fmt byterate
+    // wave.waveFormatChunk byterate
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
     printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
-    fmt.byteRate  = buffer4[0] |
+    wave.waveFormatChunk.byteRate  = buffer4[0] |
                         (buffer4[1] << 8) |
                         (buffer4[2] << 16) |
                         (buffer4[3] << 24);
-    printf("(29-32) Byte Rate: %u , Bit Rate:%u\n", fmt.byteRate, fmt.byteRate*8);
+    printf("(29-32) Byte Rate: %u , Bit Rate:%u\n", wave.waveFormatChunk.byteRate, wave.waveFormatChunk.byteRate*8);
 
     // block alignment
     read = fread(buffer2, sizeof(buffer2), 1, ptr);
     printf("%u %u \n", buffer2[0], buffer2[1]);
-    fmt.blockAlign = buffer2[0] |
+    wave.waveFormatChunk.blockAlign = buffer2[0] |
                     (buffer2[1] << 8);
-    printf("(33-34) Block Alignment: %u \n", fmt.blockAlign);
+    printf("(33-34) Block Alignment: %u \n", wave.waveFormatChunk.blockAlign);
 
     // bits per sample
     read = fread(buffer2, sizeof(buffer2), 1, ptr);
     printf("%u %u \n", buffer2[0], buffer2[1]);
-    fmt.bitsPerSample = buffer2[0] |
+    wave.waveFormatChunk.bitsPerSample = buffer2[0] |
                     (buffer2[1] << 8);
-    printf("(35-36) Bits per sample: %u \n", fmt.bitsPerSample);
+    printf("(35-36) Bits per sample: %u \n", wave.waveFormatChunk.bitsPerSample);
 
-    // chunk header marker
-    read = fread(data_chunk.chunkId, sizeof(data_chunk.chunkId), 1, ptr);
-    printf("(37-40) Data Marker: %s \n", data_chunk.chunkId);
+    // chunk wave.waveHeader marker
+    read = fread(wave.waveDataChunk.chunkId, sizeof(wave.waveDataChunk.chunkId), 1, ptr);
+    printf("(37-40) Data Marker: %s \n", wave.waveDataChunk.chunkId);
 
     // chunk size
     read = fread(buffer4, sizeof(buffer4), 1, ptr);
     printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
-    data_chunk.chunkSize = buffer4[0] |
+    wave.waveDataChunk.chunkSize = buffer4[0] |
                     (buffer4[1] << 8) |
                     (buffer4[2] << 16) |
                     (buffer4[3] << 24 );
-    printf("(41-44) Size of data chunk: %u \n", data_chunk.chunkSize);
+    printf("(41-44) Size of data chunk: %u \n", wave.waveDataChunk.chunkSize);
 
     // calculate no.of samples
-    num_samples = (8 * data_chunk.chunkSize) / (fmt.numChannels * fmt.bitsPerSample);
+    num_samples = (8 * wave.waveDataChunk.chunkSize) / (wave.waveFormatChunk.numChannels * wave.waveFormatChunk.bitsPerSample);
     printf("Number of samples:%lu \n", num_samples);
 
-    size_of_each_sample = (fmt.numChannels * fmt.bitsPerSample) / 8;
+    size_of_each_sample = (wave.waveFormatChunk.numChannels * wave.waveFormatChunk.bitsPerSample) / 8;
     printf("Size of each sample:%ld bytes\n", size_of_each_sample);
 
     // calculate duration of file
-    float duration_in_seconds = (float) header.fileSize / fmt.byteRate;
+    float duration_in_seconds = (float) wave.waveHeader.fileSize / wave.waveFormatChunk.byteRate;
     printf("Approx.Duration in seconds=%f\n", duration_in_seconds);
 
-    data_chunk.sampleData = calloc(num_samples, size_of_each_sample);
-
-    for (int i = 0; i < num_samples; i++) {
-        fread(buffer4, size_of_each_sample, 1, fp);
-        data_chunk.sampleData[i] = (buffer[0]) | (buffer[1] << 8);
+    wave.waveDataChunk.sampleData = calloc(num_samples, size_of_each_sample);
+    
+    int i;
+    for (i = 0; i < num_samples; i++) {
+        fread(buffer4, size_of_each_sample, 1, ptr);
+        wave.waveDataChunk.sampleData[i] = (buffer4[0]) | (buffer4[1] << 8);
     }
+}
+
+void convertIntToLittleEndian(__uint32_t chunk) {
+    buffer4[0] =  chunk & 0x000000FF;
+    buffer4[1] = (chunk & 0x0000FF00) >> 8;
+    buffer4[2] = (chunk & 0x00FF0000) >> 16;
+    buffer4[3] = (chunk & 0xFF000000) >> 24;
+}
+
+void convertShortToLittleEndian(__uint16_t chunk) {
+    buffer2[0] =  chunk & 0x000000FF;
+    buffer2[1] = (chunk & 0x0000FF00) >> 8;
+}
+
+
+void writeWaveFile(char *filename) {
+    FILE *o_ptr;
+    o_ptr = fopen(filename, "w");
+    if (o_ptr == NULL) {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+
+    fwrite(wave.waveHeader.riff, sizeof(wave.waveHeader.riff), 1, o_ptr);
+    
+    convertIntToLittleEndian(wave.waveHeader.fileSize); 
+    fwrite(buffer4, sizeof(buffer4), 1, o_ptr);
+
+    fwrite(wave.waveHeader.wave, sizeof(wave.waveHeader.wave), 1, o_ptr);
+
+    fwrite(wave.waveFormatChunk.chunkType, sizeof(wave.waveFormatChunk.chunkType), 1, o_ptr);
+
+    convertIntToLittleEndian(wave.waveFormatChunk.chunkSize); 
+    fwrite(buffer4, sizeof(buffer4), 1, o_ptr);
+
+    convertShortToLittleEndian(wave.waveFormatChunk.audioFormat); 
+    fwrite(buffer2, sizeof(__uint16_t), 1, o_ptr);
+
+    convertShortToLittleEndian(wave.waveFormatChunk.numChannels); 
+    fwrite(buffer2, sizeof(__uint16_t), 1, o_ptr);
+    
+    convertIntToLittleEndian(wave.waveFormatChunk.sampleRate); 
+    fwrite(buffer4, sizeof(buffer4), 1, o_ptr);
+
+    convertIntToLittleEndian(wave.waveFormatChunk.byteRate); 
+    fwrite(buffer4, sizeof(buffer4), 1, o_ptr);
+
+    convertShortToLittleEndian(wave.waveFormatChunk.blockAlign); 
+    fwrite(buffer2, sizeof(__uint16_t), 1, o_ptr);
+
+    convertShortToLittleEndian(wave.waveFormatChunk.bitsPerSample); 
+    fwrite(buffer2, sizeof(__uint16_t), 1, o_ptr);
+
+    fwrite(wave.waveDataChunk.chunkId, sizeof(wave.waveDataChunk.chunkId), 1, o_ptr);
+
+    convertIntToLittleEndian(wave.waveDataChunk.chunkSize); 
+    fwrite(buffer4, sizeof(buffer4), 1, o_ptr);
+
+    printf("Writing data...\n");
+    printf("Num Samples: %lu\n", num_samples);
+
+    int i;
+    for (i = 0; i < num_samples; i++) {
+        printf("%d\n", wave.waveDataChunk.sampleData[i]);
+        convertShortToLittleEndian(wave.waveDataChunk.sampleData[i]);
+        fwrite(buffer2, sizeof(__uint32_t), 1, o_ptr);
+    }
+
+
+    // fwrite(&wave.waveHeader, sizeof(wave.waveHeader), 1, o_ptr);
+    // fwrite(&wave.waveFormatChunk, sizeof(wave.waveFormatChunk), 1, o_ptr);
+    // fwrite(&wave.waveDataChunk, sizeof(wave.waveDataChunk), 1, o_ptr);
+    // fwrite(wave.waveDataChunk.sampleData, sizeof(wave.waveDataChunk.sampleData), 1, o_ptr);
+    fclose(o_ptr);
 }
 
 short signum ( short sample ) {
@@ -163,52 +246,52 @@ char codeword_compression ( unsigned short sample_magnitude, short sign ) {
     int codeword_tmp;
 
     if( sample_magnitude & (1 << 12)) {
-        chord = 0 x7 ;
+        chord = 0x7;
         step = ( sample_magnitude >> 8) & 0xF ;
-        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        codeword_tmp = ( sign << 7) | ( chord << 4) | step ;
         return ( (char) codeword_tmp );
     }
     if( sample_magnitude & (1 << 11)) {
-        chord = 0 x6 ;
+        chord = 0x6;
         step = ( sample_magnitude >> 7) & 0xF ;
-        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        codeword_tmp = ( sign << 7) | ( chord << 4) | step ;
         return ( (char) codeword_tmp );
     }
     if( sample_magnitude & (1 << 10)) {
-        chord = 0 x5 ;
+        chord = 0x5;
         step = ( sample_magnitude >> 6) & 0xF ;
-        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        codeword_tmp = ( sign << 7) | ( chord << 4) | step ;
         return ( (char) codeword_tmp );
     }
 
     if( sample_magnitude & (1 << 9)) {
-        chord = 0 x4 ;
+        chord = 0x4;
         step = ( sample_magnitude >> 5) & 0xF ;
-        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        codeword_tmp = ( sign << 7) | ( chord << 4) | step ;
         return ( (char) codeword_tmp );
     }
     if( sample_magnitude & (1 << 8)) {
-        chord = 0 x3 ;
+        chord = 0x3;
         step = ( sample_magnitude >> 4) & 0xF ;
-        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        codeword_tmp = ( sign << 7) | ( chord << 4) | step ;
         return ( (char) codeword_tmp );
     }
     if( sample_magnitude & (1 << 7)) {
-        chord = 0 x2 ;
+        chord = 0x2;
         step = ( sample_magnitude >> 3) & 0xF ;
-        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        codeword_tmp = ( sign << 7) | ( chord << 4) | step ;
         return ( (char) codeword_tmp );
     }
     if( sample_magnitude & (1 << 6)) {
-        chord = 0 x1 ;
+        chord = 0x1;
         step = ( sample_magnitude >> 2) & 0xF ;
-        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        codeword_tmp = ( sign << 7) | ( chord << 4) | step ;
         return ( (char) codeword_tmp );
     }
     if( sample_magnitude & (1 << 5)) {
-        chord = 0 x0 ;
+        chord = 0x0;
         step = ( sample_magnitude >> 1) & 0xF ;
-        codeword_tmp = ( sign << 7) & ( chord << 4) & step ;
+        codeword_tmp = ( sign << 7) | ( chord << 4) | step ;
         return ( (char) codeword_tmp );
     }
 }
@@ -251,9 +334,12 @@ void compressData () {
     // TODO add error checking
     compressed_data.sampleData = calloc(num_samples, size_of_each_sample);
 
-    for (int i = 0; i < num_samples; i++) {
+    printf("Compressing data...\n");
+    
+    int i;
+    for ( i = 0; i < num_samples; i++) {
         // get sampple
-        short sample = (data_chunk.sampleData[i] >> 2);
+        short sample = (wave.waveDataChunk.sampleData[i] >> 2);
         // get sign
         short sign = signum(sample);
         // get magnitude
@@ -262,6 +348,7 @@ void compressData () {
         char codeword = codeword_compression(magninute, sign);
         // flip codeword
         codeword = ~codeword;
+        printf("Codeword: %hu\n", codeword);
         // save
         compressed_data.sampleData[i] = codeword;
     }
@@ -270,12 +357,19 @@ void compressData () {
 void decompressData () {
     // TODO add error checking
 
+    printf("Decompressing data...\n");
+
     __uint8_t codeword;
-    for (int i = 0; i < numSamples; i++) {
+    int i;
+    for (i = 0; i < num_samples; i++) {
+        printf("%hu\n", compressed_data.sampleData[i]);
         codeword = ~(compressed_data.sampleData[i]);
         short sign = (codeword & 0x80) >> 7;
+        printf("%d\n", sign);
         unsigned short magnitude = (magnitude_decode(codeword) - 33);
+        printf("%d\n", magnitude);
         short sample = (short) (sign ? magnitude : -magnitude);
+        printf("%d\n", sample << 2);
         wave.waveDataChunk.sampleData[i] = sample << 2;
     }
 }
