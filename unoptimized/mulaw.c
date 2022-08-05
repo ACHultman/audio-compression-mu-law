e35d219198183a563b24f1cd1a01ccb791d8ee2b
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-08-05 00:52:20 -0700

    partial convert unopt, hardware to use barr-c standard

--- unoptimized/mulaw.c
+++ unoptimized/mulaw.c
@@ -11,7 +11,8 @@
  */
 void compress() {
 	compressed_wave.samples = calloc(num_samples, sizeof(uint8_t));
-	if (compressed_wave.samples == NULL) {
+	if (compressed_wave.samples == NULL) 
+	{
 		printf("Could not allocate memory for compressed samples.\n");
 		exit(1);
 	}
@@ -22,7 +23,8 @@ void compress() {
 	uint8_t codeword; // 8-bit unsigned integer - MuLaw codeword for sample
 
 	int i;
-	for (i = 0; i < num_samples; i++) {
+	for (i = 0; i < num_samples; i++)
+	{
 		sample = (wave.samples[i] >> 2); // right shift by 2 to get rid of last 2 bits (only 1 sign bit, 13 magnitude bits are used)
 		sign = signum(sample); // get sign of sample
 		mag = magnitude(sample) + 33; // get magnitude of sample and add 33 so that it is a power of 2
@@ -53,39 +55,48 @@ uint8_t find_codeword(uint8_t sign, uint16_t mag) {
     // - return codeword directly instead of wasting time on the if-else statement
 
 	// Chord corresponds to the location of the msb of 1 in the magnitude
-	if (mag & (1 << 12)) {
+	if (mag & (1 << 12)) 
+	{
 		chord = 0x7;
 		step = (mag >> 8) & 0xF;
 	}
-	else if (mag & (1 << 11)) {
+	else if (mag & (1 << 11)) 
+	{
 		chord = 0x6;
 		step = (mag >> 7) & 0xF;
 	}
-	else if (mag & (1 << 10)) {
+	else if (mag & (1 << 10)) 
+	{
 		chord = 0x5;
 		step = (mag >> 6) & 0xF;
 	}
-	else if (mag & (1 << 9)) {
+	else if (mag & (1 << 9)) 
+	{
 		chord = 0x4;
 		step = (mag >> 5) & 0xF;
 	}
-	else if (mag & (1 << 8)) {
+	else if (mag & (1 << 8)) 
+	{
 		chord = 0x3;
 		step = (mag >> 4) & 0xF;
 	}
-	else if (mag & (1 << 7)) {
+	else if (mag & (1 << 7)) 
+	{
 		chord = 0x2;
 		step = (mag >> 3) & 0xF;
 	}
-	else if (mag & (1 << 6)) {
+	else if (mag & (1 << 6)) 
+	{
 		chord = 0x1;
 		step = (mag >> 2) & 0xF;
 	}
-	else if (mag & (1 << 5)) {
+	else if (mag & (1 << 5)) 
+	{
 		chord = 0x0;
 		step = (mag >> 1) & 0xF;
 	}
-	else {
+	else 
+	{
 		printf("Invalid magnitude while compressing.\n");
 		printf("Magnitude: %d\n", mag);
 		exit(1);
@@ -121,7 +132,8 @@ uint8_t compressed_signum(uint8_t codeword) {
  * @return uint16_t - magnitude of sample
  */
 uint16_t magnitude(int16_t sample) {
-	if (sample < 0) {
+	if (sample < 0) 
+	{
 		sample = -sample;
 	}
 
@@ -141,31 +153,40 @@ uint16_t compressed_magnitude(uint8_t codeword) {
     // TODO possible optimizations:
     // - use switch statement instead of if-else statement
 
-	if (chord == 0x7) {
+	if (chord == 0x7) 
+	{
 		return (1 << 12) | (step << 8) | (1 << 7);
 	}
-	else if (chord == 0x6) {
+	else if (chord == 0x6) 
+	{
 		return (1 << 11) | (step << 7) | (1 << 6);
 	}
-	else if (chord == 0x5) {
+	else if (chord == 0x5) 
+	{
 		return (1 << 10) | (step << 6) | (1 << 5);
 	}
-	else if (chord == 0x4) {
+	else if (chord == 0x4) 
+	{
 		return (1 << 9) | (step << 5) | (1 << 4);
 	}
-	else if (chord == 0x3) {
+	else if (chord == 0x3) 
+	{
 		return (1 << 8) | (step << 4) | (1 << 3);
 	}
-	else if (chord == 0x2) {
+	else if (chord == 0x2) 
+	{
 		return (1 << 7) | (step << 3) | (1 << 2);
 	}
-	else if (chord == 0x1) {
+	else if (chord == 0x1) 
+	{
 		return (1 << 6) | (step << 2) | (1 << 1);
 	}
-	else if (chord == 0x0) {
+	else if (chord == 0x0) 
+	{
 		return (1 << 5) | (step << 1) | 1;
 	}
-	else {
+	else 
+	{
 		printf("Invalid chord while decompressing.\n");
 		printf("Chord: %d\n", chord);
 		exit(1);
@@ -186,7 +207,8 @@ void decompress() {
     // - use bitwise operations instead of logical operations
 
 	int i;
-	for (i = 0; i < num_samples; i++) {
+	for (i = 0; i < num_samples; i++) 
+	{
 		codeword = compressed_wave.samples[i];
 		codeword = ~codeword;
 		sign = compressed_signum(codeword);
